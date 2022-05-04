4923af2f3648edaed960d71e27fa6fe79e4de1f0
Borovinskiy Pavel <borovinskiy.job@gmail.com>
2022-05-04 10:30:14 +0300

    reack-shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Items from './components/Items';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [{
+				id: 1,
+				title: 'Стул серый',
+				img: 'chairs-grey.jpg',
+				desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, distinctio',
+				category: 'chairs',
+				price: '49.99'
+			},
+			{
+				id: 2,
+				title: 'Стол',
+				img: 'table.jpg',
+				desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, distinctio',
+				category: 'tables',
+				price: '49.99'
+			},
+			{
+				id: 3,
+				title: 'Кравать',
+				img: 'bed.jpg',
+				desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, distinctio',
+				category: 'beds',
+				price: '49.99'
+			},
+			{
+				id: 4,
+				title: 'Люстра',
+				img: 'chandelier.jpg',
+				desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, distinctio',
+				category: 'chandeliers',
+				price: '49.99'
+			},
+			{
+				id: 5,
+				title: 'Диван',
+				img: 'sofa.jpg',
+				desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, distinctio',
+				category: 'sofas',
+				price: '49.99'
+			},
+			{
+				id: 6,
+				title: 'Шкаф',
+				img: 'closet.jpg',
+				desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, distinctio',
+				category: 'closets',
+				price: '49.99'
+			},
+			]
+		}
+	}
+
+	render() {
+		return (
+			<div className='wrapper'>
+				<Header />
+				<Items items={this.state.items} />
+				<Footer />
+			</div>
+		)
+	}
+
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+	return (
+		<header>
+			<div>
+				<span className='logo'>House staff</span>
+				<ul className='nav'>
+					<li>Про нас</li>
+					<li>Контакты</li>
+					<li>Кабинет</li>
+				</ul>
+			</div>
+			<div className='presentation'></div>
+		</header>
+
+
+	)
+}
+
+
